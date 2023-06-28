2f5316ad1ecdeb48135e36ec4c913517cd6e3410
Sergiu Pana <sergiu.pana@softwire.com>
2023-06-28 16:53:43 +0300

    Made pawns move up one square, if possible.

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,22 @@ export default class Pawn extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const availableMoves = new Array<Square>()
+        const currentPosition = board.findPiece(this);
+        let squareInFront: Square;
+
+        if (this.player === Player.WHITE) {
+            squareInFront = Square.at(currentPosition.row + 1, currentPosition.col);
+        } else {
+            squareInFront = Square.at(currentPosition.row - 1, currentPosition.col);
+        }
+
+        const pieceInFront = board.getPiece(squareInFront);
+
+        if (!pieceInFront) {
+            availableMoves.push(squareInFront);
+        }
+
+        return availableMoves;
     }
 }
